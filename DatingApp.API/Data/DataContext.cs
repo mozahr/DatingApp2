37216b04968cd5546e4819a6e64982720739612c
Mozahr <mohamed_z91@hotmail.com>
2019-08-17 19:39:52 +0300

    End Of Section 3

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -7,6 +7,9 @@ namespace DatingApp.API.Data
     {
         public DataContext(DbContextOptions<DataContext> options): base (options){}
 
+    //Data context is modelfirst generated, you need to add the model class as 
+    //a dbset and make a dotnet database update to update the database
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
